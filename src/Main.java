f7160ee38acf7ce51e82c407a78284cb972146e7
oyasnev <oyasnev@gmail.com>
2014-03-11 23:49:53 +0400

    Project created

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    String s = "  d   fgfg|fgg  d f";
+        String[] arr = s.split("\\s+");
+        System.out.println();
+    }
+}

6f477a188554ebeb22e8669cba43826c96d5295d
oyasnev <oyasnev@gmail.com>
2014-03-25 00:18:15 +0400

    ParamParser

--- src/Main.java
+++ src/Main.java
@@ -17,29 +17,37 @@ import java.util.ArrayList;
 
 public class Main {
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
+        ParamParser params = new ParamParser(args);
+        if (!params.state) {
+            return;
+        }
+
         StdOut.println("Merge repeats...");
-        //In input = new In("merge_test.out");
-        //In input = new In("D.melanogaster_chrX.fna.out");
-        In input = new In("chrX.fa.out");
-        Out output = new Out("merge_test_output.out");
-        MergeFilter mergeFilter = new MergeFilter(input, output, 20);
+        In input = new In(params.repeatFile);
+        Out output = new Out("~merge_" + params.repeatFile);
+        MergeFilter mergeFilter = new MergeFilter(input, output, params.mergeThreshold);
+        input.close();
+        output.close();
+
+        // filter
         ArrayList<RepeatLine> filteredRepeats = RepeatFilter.filter(mergeFilter.rlArr);
         RepeatLine[] rlArr =  new RepeatLine[filteredRepeats.size()];
         filteredRepeats.toArray(rlArr);
 
-        // read sequences
         StdOut.println("Read sequence...");
         SequenceFactory sequenceFactory = new SequenceFactory();
-        //ArrayList<Sequence> arList = (ArrayList<Sequence>) Fasta.readSequences("D.melanogaster_chrX.fna", sequenceFactory);
-        ArrayList<Sequence> arList = (ArrayList<Sequence>) Fasta.readSequences("chrX.fa", sequenceFactory);
+        ArrayList<Sequence> arList = (ArrayList<Sequence>) Fasta.readSequences(params.inputSeqFile, sequenceFactory);
 
         StdOut.println("Find TSD...");
-        TSDFind tsdFind = new TSDFind(rlArr, arList.get(0), 7, 3);
+        TSDFind tsdFind = new TSDFind(rlArr, arList.get(0), params.tsdLength, params.tsdEditDistance);
         StdOut.println("TSD found: " + tsdFind.getTSDList().size());
 
         StdOut.println("Write TSD to file...");
-        TSDFile.write(tsdFind.getTSDList(), new Out("tsd.txt"));
+        output = new Out(params.outputFile);
+        TSDFile.write(tsdFind.getTSDList(), output);
+        output.close();
     }
 
 
---
+++ src/ParamParser.java
@@ -0,0 +1,105 @@
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdOut;
+
+/**
+ * Author: Oleg Yasnev (oyasnev@gmail.com)
+ * Date: 25.11.13
+ */
+
+public class ParamParser {
+    public static final String INPUT_FILE_KEY        = "-i";
+    public static final String REPEAT_FILE_KEY       = "-r";
+    public static final String OUTPUT_FILE_KEY       = "-o";
+    public static final String MERGE_KEY             = "-m";
+    public static final String MERGE_THRESHOLD_KEY   = "-mt";
+    public static final String TSD_LENGTH_KEY        = "-tl";
+    public static final String TSD_EDIT_DISTANCE_KEY = "-ted";
+
+    public static final String README = "readme.txt";
+
+    public boolean state = false;
+
+    public String  inputSeqFile     = "";
+    public String  repeatFile       = "";
+    public String  outputFile       = "tsd.txt";
+    public boolean merge            = false;
+    public int     mergeThreshold   = 20;
+    public int     tsdLength        = 7;
+    public int     tsdEditDistance  = 3;
+
+    public ParamParser(String[] args) {
+        if (args.length == 0 || args[0].equals("--help") || args[0].equals("?") || args[0].equals("-h")) {
+            printHelp();
+            return;
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            String key = args[i];
+            if      (key.equals(INPUT_FILE_KEY)) {
+                i++;
+                inputSeqFile = args[i];
+            }
+            else if (key.equals(OUTPUT_FILE_KEY)) {
+                i++;
+                outputFile = args[i];
+            }
+            else if (key.equals(REPEAT_FILE_KEY)) {
+                i++;
+                repeatFile = args[i];
+            }
+            else if (key.equals(MERGE_KEY)) {
+                merge = true;
+            }
+            else if (key.equals(MERGE_THRESHOLD_KEY)) {
+                i++;
+                mergeThreshold = Integer.parseInt(args[i]);
+            }
+            else if (key.equals(TSD_LENGTH_KEY)) {
+                i++;
+                tsdLength = Integer.parseInt(args[i]);
+            }
+            else if (key.equals(TSD_EDIT_DISTANCE_KEY)) {
+                i++;
+                tsdEditDistance = Integer.parseInt(args[i]);
+            }
+        }
+
+        // check state
+        state = true;
+        if (inputSeqFile.isEmpty()) {
+            state = false;
+            StdOut.println("Input sequence file must be specified");
+        }
+        if (repeatFile.isEmpty()) {
+            state = false;
+            StdOut.println("Input repeat file must be specified");
+        }
+        if (outputFile.isEmpty()) {
+            state = false;
+            StdOut.println("Output file must be specified");
+        }
+        if (mergeThreshold < 0) {
+            state = false;
+            StdOut.println("Merge threshold must be >= 0");
+        }
+        if (tsdLength <= 0) {
+            state = false;
+            StdOut.println("TSD minimum length must be > 0");
+        }
+        if (tsdEditDistance < 0) {
+            state = false;
+            StdOut.println("TSD maximum edit distance must be >= 0");
+        }
+
+        if (!state) {
+            StdOut.println("For more information run the program with the key '--help'");
+        }
+    }
+
+    public static void printHelp() {
+        In in = new In(README);
+        while (!in.isEmpty()) {
+            StdOut.println(in.readLine());
+        }
+    }
+}
